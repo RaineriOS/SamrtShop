4218f4e2747834cd8e609bf2965f5ad4b3d71fcd
Batman <macowner@Dont-Just-Dont.local>
2013-11-05 09:43:35 +0000

    Initial Commit

---
+++ SmartShop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartShop
+//
+//  Created by Batman on 05/11/2013.
+//  Copyright (c) 2013 Batman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartShop/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmartShop
+//
+//  Created by Batman on 05/11/2013.
+//  Copyright (c) 2013 Batman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
