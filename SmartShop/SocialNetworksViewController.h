1110587b2b519b98a0790356670112ad2ba7634b
Batman <macowner@Dont-Just-Dont.local>
2013-11-14 18:04:45 +0000

    Added the map view to the social view which can show the current location of the user or pick a new location.

--- SmartShop/SocialNetworksViewController.h
+++ SmartShop/SocialNetworksViewController.h
@@ -7,12 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface SocialNetworksViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextViewDelegate>
+@interface SocialNetworksViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextViewDelegate,
+    CLLocationManagerDelegate>
 
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (strong, nonatomic) IBOutlet UITextView *textViewPostContent;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 - (IBAction)takePicture:(id)sender;
 - (IBAction)tweet:(id)sender;
 - (IBAction)post:(id)sender;
--- SmartShop/SocialNetworksViewController.m
+++ SmartShop/SocialNetworksViewController.m
@@ -14,9 +14,13 @@
 
 @interface SocialNetworksViewController ()
 @property (nonatomic, strong) ACAccountStore *accountStore;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) MKPointAnnotation *annotationView;
 @end
 
 @implementation SocialNetworksViewController
+
+@synthesize locationManager;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -53,6 +57,15 @@
     resigneKeyboardTapRecognizer.numberOfTapsRequired = 1;
     [self.view addGestureRecognizer:resigneKeyboardTapRecognizer];
 
+    // Map click handler
+    UILongPressGestureRecognizer *recognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(addPin:)];
+    //[recognizer setNumberOfTapsRequired:1];
+    [self.mapView addGestureRecognizer:recognizer];
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation; // 100 m
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
 }
 
 -(void)resignTextViewPostContentKeyboard:(UIGestureRecognizer *)getureRecognizer
@@ -254,4 +267,66 @@
     }
 }
 
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    // If it's a relatively recent event, turn off updates to save power.
+    CLLocation* location = [locations lastObject];
+    NSDate* eventDate = location.timestamp;
+    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
+    if (abs(howRecent) < 15.0) {
+        // If the event is recent, do something with it.
+        NSLog(@"latitude %+.6f, longitude %+.6f\n",
+              location.coordinate.latitude,
+              location.coordinate.longitude);
+    }
+   if (self.annotationView)
+       [self.mapView removeAnnotation:self.annotationView];
+    self.annotationView = [[MKPointAnnotation alloc] init];
+    self.annotationView.coordinate = location.coordinate;
+    [self placeAnnotationOnMap:self.annotationView];
+}
+
+#pragma mark - update map view UI
+// Add pin to the map
+- (void)addPin:(UITapGestureRecognizer*)recognizer
+{
+    [self.locationManager stopUpdatingLocation];
+    CGPoint tappedPoint = [recognizer locationInView:self.mapView];
+    NSLog(@"Tapped At : %@",NSStringFromCGPoint(tappedPoint));
+    CLLocationCoordinate2D coord= [self.mapView convertPoint:tappedPoint toCoordinateFromView:self.mapView];
+    NSLog(@"lat  %f",coord.latitude);
+    NSLog(@"long %f",coord.longitude);
+    MKPointAnnotation *annotationView = [[MKPointAnnotation alloc] init];
+    annotationView.coordinate = CLLocationCoordinate2DMake(coord.latitude, coord.longitude);
+    if (self.annotationView)
+        [self.mapView removeAnnotation:self.annotationView];
+    self.annotationView = annotationView;
+    [self placeAnnotationOnMap:self.annotationView];
+    // add an annotation with coord
+}
+
+-(void)placeAnnotationOnMap:(MKPointAnnotation *)annotationView
+{
+    [self.mapView addAnnotation:(id)annotationView];
+    
+    MKMapCamera *camera = [MKMapCamera
+                           cameraLookingAtCenterCoordinate:annotationView.coordinate
+                           fromEyeCoordinate:annotationView.coordinate
+                           eyeAltitude:70000.0];
+    
+    [UIView animateWithDuration:1.0 animations:^{
+        [self.mapView setCamera:camera];
+    }];
+}
 @end
